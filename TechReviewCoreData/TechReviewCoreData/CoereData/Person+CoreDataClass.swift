ac3adec3ea310157c17eb4ede7a88460f5677470
hunter.liu <hunter.liu@homecreditcfc.cn>
2018-09-07 17:43:57 +0800

    core data initial revision

---
+++ TechReviewCoreData/TechReviewCoreData/CoereData/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  
+//
+//  Created by jhunter on 2018/9/7.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
---
+++ TechReviewCoreData/TechReviewCoreData/CoereData/Person+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Person+CoreDataProperties.swift
+//  
+//
+//  Created by jhunter on 2018/9/7.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Person {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Person> {
+        return NSFetchRequest<Person>(entityName: "Person")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var age: Int16
+    @NSManaged public var address: String?
+    @NSManaged public var gender: Bool
+    @NSManaged public var id: Int64
+    @NSManaged public var cellphone: String?
+
+}
